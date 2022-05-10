1a5fc1bfc733084d622900517022470f328e2aee
justguang <2320366520@qq.com>
2022-05-10 17:02:10 +0800

    add file => Test_ULog.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,53 +3,14 @@ using ULogs;
 
 namespace Test
 {
-    class Root
-    {
-        public void Init()
-        {
-            this.Log("Init Root Log.");
-            Mgr mgr = new Mgr();
-            mgr.Init();
-        }
-    }
-
-    class Mgr
-    {
-        public void Init()
-        {
-            this.Warn("Init Mgr Warn");
-            Item item = new Item();
-            item.Init();
-        }
-    }
-
-    class Item
-    {
-        public void Init()
-        {
-            this.Error("Init Item Error");
-            this.Trace("Trace this Func");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            ULog.InitSetting();
-
-            ULog.Log("【Log】  六六六!!! {0}", "ServerLCGLog");
-            ULog.ColorLog(ULogColor.Blue, "【ColorLog】  六六六!!! color=> {0}={1}", "ColorLog.Blue", ULogColor.Blue);
-            ULog.ColorLog(ULogColor.Green, "【ColorLog】 六六六!!! color=> {0}={1}", "ColorLog.Green", ULogColor.Green);
-            ULog.ColorLog(ULogColor.Cyan, "【ColorLog】  六六六!!! color=> {0}={1}", "ColorLog.Cyan", ULogColor.Cyan);
-            ULog.ColorLog(ULogColor.Magenta, "【ColorLog】 六六六!!! color=> {0}={1}", "ColorLog.Magenta", ULogColor.Magenta);
-            ULog.ColorLog(ULogColor.Yellow, "【ColorLog】 六六六!!! color=> " + "ColorLog.Yellow=" + ULogColor.Yellow);
-            ULog.ColorLog(ULogColor.Red, "【ColorLog】 六六六!!! color=> {0}", "ColorLog.Red=" + ULogColor.Red);
-
-
-
-            Root r = new Root();
-            r.Init();
+            //## test ULog
+            Test_ULog test_ULog = new Test_ULog();
+            test_ULog.Init();
+            //end
 
 
             Console.WriteLine("Hello World!");
