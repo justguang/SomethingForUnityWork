b9579d3e73bf26fbaa303c56cefece1717f61b9b
justguang <2320366520@qq.com>
2022-05-13 17:27:22 +0800

    add project UKCPExampleProtocol

---
+++ UKCPExampleProtocol/NetMsg.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+///********************************************
+/// ClassName    ：  NetMsg
+/// Author       ：  LCG
+/// CreateTime   ：  2022/5/13 星期五 
+/// Description  ：  UKCP 网络通信数据协议
+///********************************************/
+/// </summary>
+using System;
+using UKcps;
+
+namespace KCPExampleProtocol
+{
+    [Serializable]
+    public class NetMsg:KCPMsg
+    {
+        public CMD cmd;
+        public NetPing netPing;
+        public string info;
+    }
+
+    [Serializable]
+    public class NetPing
+    {
+        //心跳，true => 连接结束
+        public bool isOver;
+    }
+
+    public enum CMD
+    {
+        None,
+        NetPing,
+    }
+}
