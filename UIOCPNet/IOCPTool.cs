275abf58741544825011fd6a19cb3a5b0a66f447
justguang <2320366520@qq.com>
2022-06-20 16:33:24 +0800

    add project【UIOCPNet】

---
+++ UIOCPNet/IOCPTool.cs
@@ -0,0 +1,79 @@
+﻿/// <summary>
+///********************************************
+/// ClassName    ：  IOCPTool
+/// Author       ：  LCG
+/// CreateTime   ：  2022/6/20 星期一
+/// Description  ：  工具函数
+///********************************************/
+/// </summary>
+using System;
+using System.Threading;
+
+namespace UUIOCPNet
+{
+    /// <summary>
+    /// 工具函数
+    /// </summary>
+    public static class IOCPTool
+    {
+        #region LOG
+        public static void ConsoleLog(string msg, IOCPLogColor color)
+        {
+            int threadID = Thread.CurrentThread.ManagedThreadId;
+            msg = string.Format("ThreadID:{0} {1}", threadID, msg);
+            switch (color)
+            {
+                case IOCPLogColor.Red:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case IOCPLogColor.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case IOCPLogColor.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case IOCPLogColor.Cyan:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case IOCPLogColor.Magenta:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case IOCPLogColor.Yellow:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case IOCPLogColor.None:
+                default:
+                    break;
+            }
+        }
+        #endregion
+
+
+    }
+
+    /// <summary>
+    /// 日志颜色
+    /// </summary>
+    public enum IOCPLogColor
+    {
+        None,
+        Red,
+        Green,
+        Blue,
+        Cyan,
+        Magenta,
+        Yellow,
+    }
+}
