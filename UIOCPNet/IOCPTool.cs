2317c44b5c98e1dff3b4d2b817ef63d52e7c4c91
justguang <2320366520@qq.com>
2023-01-11 17:03:25 +0800

    update

--- UIOCPNet/IOCPTool.cs
+++ UIOCPNet/IOCPTool.cs
@@ -1,4 +1,4 @@
-﻿/// <summary>
+/// <summary>
 ///********************************************
 /// ClassName    ：  IOCPTool
 /// Author       ：  LCG
--- UIOCPNet/UIOCPNet_Example.cs
+++ UIOCPNet/UIOCPNet_Example.cs
@@ -1,4 +1,4 @@
-﻿/// <summary>
+/// <summary>
 ///********************************************
 /// ClassName    ：  UIOCPNet_Example
 /// Author       ：  LCG
