66d751aff48fb36b98778f7aada82a9c54187375
justguang <2320366520@qq.com>
2023-01-11 17:02:35 +0800

    update

--- UKCP/KCPSession.cs
+++ UKCP/KCPSession.cs
@@ -1,4 +1,4 @@
-﻿/// <summary>
+/// <summary>
 ///********************************************
 /// ClassName    ：  KCPSession
 /// Author       ：  LCG
--- UKCP/UKCPTool.cs
+++ UKCP/UKCPTool.cs
@@ -1,4 +1,4 @@
-﻿/// <summary>
+/// <summary>
 ///********************************************
 /// ClassName    ：  UKCPTool
 /// Author       ：  LCG
